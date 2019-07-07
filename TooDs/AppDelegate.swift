8c19f743ffb6bdd1fe0b7c790b515afee69caaf0
Asim <asemokbey@gmail.com>
2019-07-07 08:33:38 +0300

    initial commit

--- TooDs/AppDelegate.swift
+++ TooDs/AppDelegate.swift
@@ -13,7 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         Thread.sleep(forTimeInterval: 0.5)
         self.window = UIWindow(frame: UIScreen.main.bounds)
